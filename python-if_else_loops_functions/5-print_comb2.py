fd45f92d2e6d3c1a8fb24f951a37ba64c8aed357
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-23 05:20:23 -0800

    :02d

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if (i < 10):
-        print("{0}".format(f"0{i},"), end=" ")
-    elif (i < 99):
-        print("{0}".format(f"{i},"), end=" ")
+    if (i < 99):
+        print("{:02d}".format(i), end=", ")
     else:
-        print("{0}".format(f"{i}"), end=" ")
+        print("{:02d}".format(i))
