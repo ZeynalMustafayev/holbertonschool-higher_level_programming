729c2d187320a3e617f9dc81a146cc5edb9daaf0
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-23 06:23:52 -0800

    new line

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if (i < j):
-            print("{}{}".format(i, j), end=", " if i != 8 or j != 9 else "")
+            print("{}{}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
         if (i == j):
             continue
