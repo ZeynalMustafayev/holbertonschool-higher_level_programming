cd268d89ddb152a5c94452c3be05163db9065b7a
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-24 06:03:29 -0800

    fix pycode error

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if (number > 0):
         number1 = number % 10
     else:
-         number1 = -1 * number % 10
+        number1 = -1 * number % 10
     print("{}".format(number1), end="")
     return (number1)
