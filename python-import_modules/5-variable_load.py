b5c6499e0b3d98234348403d5a911db818a95fa1
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-25 05:30:39 -0800

    check how work *

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#if __name__ == "__main__":
-from variable_load_5 import a
-a = 98
-print("{}".format(a))
+if __name__ == "__main__":
+    from variable_load_5 import *
+    a = 98
+    print("{}".format(a))
