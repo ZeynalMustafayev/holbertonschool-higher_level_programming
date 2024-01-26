6e38c9f16acf56c5150053b6f429dd4a5c0bdd58
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-26 05:54:03 -0800

    bug fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             if j != len(i) - 1:
-                print("{:d} ".format(i[j]), end="")
+                print("{:d}".format(i[j]), end=" ")
             else:
-                print("{:d} ".format(i[j]))
+                print("{:d}".format(i[j]))
     if matrix == [[]]:
         print("")
