e6a79a1dff707b7e9cefee176f44ebd0290c873e
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-26 04:38:00 -0800

    add append

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if 0 <= idx or idx <= len(my_list):
-        new_list = my_list[::]
-        new_list[idx] = element
-        return (new_list)
-    else:
-        return (my_list)
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(my_list[i])
+    new_list[idx] = element
+    return new_list
