e0ecfefeaca7262ac4339789607bab74c07104ec
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-27 09:47:39 -0800

    fix error

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list[idx] == 0 or idx < 0 or idx >= my_list[idx]:
+    iflen( my_list) == 0 or idx < 0 or idx >= len(my_list):
         return (my_list)
-    del my_list[idx]
+    if idx < len(my_list):
+        del my_list[idx]
     return (my_list)
