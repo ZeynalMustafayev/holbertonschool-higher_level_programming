853afdad955941690c30cc79ec70aa5be6197290
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-27 08:40:41 -0800

    i forgot include , in return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@ def multiple_returns(sentence):
     if (len_string > 0):
         i = sentence[0]
     else:
-        return (None)
+        return (None,)
     new_tuple = (len_string, i)
     return (new_tuple)
