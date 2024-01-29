640b6ec69e2cce1cc65f6810f6556e10adf8ff2f
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-29 07:26:26 -0800

    write with dict

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-def value(r):
-	if (r == 'I'):
-		return 1
-	if (r == 'V'):
-		return 5
-	if (r == 'X'):
-		return 10
-	if (r == 'L'):
-		return 50
-	if (r == 'C'):
-		return 100
-	if (r == 'D'):
-		return 500
-	if (r == 'M'):
-		return 1000
-	return (0)
-
 def roman_to_int(roman_string):
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
     res = 0
-    i = 0
-    if roman_string not a str or None:
-        return (0)
-    while (i < len(roman_string)):
-        s1 = value(roman_string[i])
-        if (i + 1 < len(roman_string)):
-            s2 = value(roman_string[i + 1])
-            if (s1 >= s2):
-                res = res + s1
-                i = i + 1
-            else:
-                res = res + s2 - s1
-                i = i + 2
+    prev_value = 0
+    for symbol in reversed(roman_string):
+        current_value = roman_values[symbol]
+        if current_value >= prev_value:
+            res += current_value
         else:
-            res = res + s1
-            i = i + 1
-    return (res)
+            res -= current_value
+        prev_value = current_value
+
+    return res
