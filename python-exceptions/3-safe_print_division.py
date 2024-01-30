f13d29e9eae64f18a71c8a0329112332ecb21808
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-30 06:54:10 -0800

    additional None problem solved

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    c = None
     try:
-        c = a/b
+        c = a / b
         return c
-        #print("{}".format(c))
     except ZeroDivisionError:
-        print(None)
+        c = None
     finally:
         print("Inside result: {}".format(c))
