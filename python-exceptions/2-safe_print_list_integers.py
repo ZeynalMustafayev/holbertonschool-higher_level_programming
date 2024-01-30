c148ca5af1496d05c915fcaf5a890bc945f93393
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-30 06:16:16 -0800

    Pycodestyle validation

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     value_count = 0
     try:
         for i in range(x):
-            if type(my_list[i]) == int:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 value_count += 1
     except (ValueError, IndexError):
