759eb7b480e5798f75266f22aef4966304092b20
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-02 06:45:29 -0800

    fix module and pyc error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""0-add_integer.py and tests/0-add_integer.txt"""
+
 
 def add_integer(a, b=98):
+    """
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int (b)
+    return int(a) + int(b)
