2064c9cd203bc5eb10fa464107d03ea3b1e9011d
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-02 09:45:04 -0800

    fix module error

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""function that prints a text with 2 new lines after each of /
+these characters: ., ? and :"""
+
+
 def text_indentation(text):
     """
     function that prints a text with 2 new lines after each /
