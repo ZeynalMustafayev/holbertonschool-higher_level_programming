d701e0c26f1f60b11b9f54b348abcba2661b984a
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-02 09:42:58 -0800

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """
+    function that prints a text with 2 new lines after each /
+    of these characters: ., ? and :
+    unit tests located in tests/5-text_indentation.txt
+    checks for type errors
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    elements = [".", "?", ":"]
+    lines = []
+    current_line = ""
+
+    for char in text:
+        current_line += char
+        if char in elements:
+            lines.append(current_line.strip())
+            current_line = ""
+
+    for line in lines:
+        print(line)
+        print()
