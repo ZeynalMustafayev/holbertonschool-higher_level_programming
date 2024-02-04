534bd8b8bc5e1db4dcfdb11abfb496ac7ac32eff
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-04 08:39:25 -0800

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""function that prints a text with 2 new lines after each of /
-these characters: ., ? and :"""
+"""
+    The ``4. Text indentation`` module
+    This module contains text_indentation function.
+"""
 
 
 def text_indentation(text):
     """
-    function that prints a text with 2 new lines after each /
-    of these characters: ., ? and :
-    unit tests located in tests/5-text_indentation.txt
-    checks for type errors
+        text_indentation - seperate text.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    elements = [".", "?", ":"]
-    lines = []
-    current_line = ""
-
-    for char in text:
-        current_line += char
-        if char in elements:
-            lines.append(current_line.strip())
-            lines.append("")
-            current_line = ""
-
-    if current_line:
-        lines.append(current_line.strip())
-
-    print('\n'.join(lines), end='')
+    text_list = []
+    single_sentence = ""
+    for letter in text:
+        single_sentence += letter
+        if letter == "." or letter == "?" or letter == ":":
+            text_list.append(single_sentence)
+            single_sentence = ""
+    text_list.append(single_sentence)
+    edited_list = []
+    for txt in text_list:
+        txt = txt.strip()
+        edited_list.append(txt)
+    output = "\n\n".join(edited_list)
+    print(output, end="")
