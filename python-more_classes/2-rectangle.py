5c5d28afe968ced9aa1e6f5d4ce693b111e87471
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-05 06:54:52 -0800

    Area and Perimeter

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Rectangle:
+    """Rectangle class with private attributes width and height"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize Rectangle with width and height attributes"""
+        self.height = height
+        self.width = width
+        
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        return (self.width + self.height) * 2
