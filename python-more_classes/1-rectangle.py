795defbbdcd4a912a7059e9d853e8d7bbe0e1d3a
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-06 04:51:22 -0800

    fix errors

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""
+    This file contains Rectangle class.
+"""
 
 
 class Rectangle:
-    """Rectangle class with private attributes width and height"""
-    number_of_instances = 0
-    print_symbol = "#"
-
+    """Rectangle Class"""
     def __init__(self, width=0, height=0):
-        """Initialize Rectangle with width and height attributes"""
-        self.height = height
+        """initialize parameters"""
         self.width = width
-        Rectangle.number_of_instances += 1
+        self.height = height
+
+    @property
+    def width(self):
+        # Get the value width
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        # Set the value width
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
+        # Get the value height
         return self.__height
 
     @height.setter
     def height(self, value):
+        # Set the value height
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
