94ab69d889d12806d0b0166636529059b3dd9981
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-07 23:57:31 -0800

    fix module error

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""a function  returns the list of available attributes and methods"""
+
+
 class MyList(list):
+    """mylist function"""
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
