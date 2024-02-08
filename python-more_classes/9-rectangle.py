6b962b2145ace53b5a22d5966a22e6185ab59dcf
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 09:06:47 -0800

    change place height and width

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -9,9 +9,22 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize Rectangle with width and height attributes"""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -20,24 +33,11 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = value
+            self.__height = value
 
     def area(self):
         return (self.width * self.height)
