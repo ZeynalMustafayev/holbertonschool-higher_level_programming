c43bcdf8b161893f4ee0e02c8aaf14091cc0a83f
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 01:07:42 -0800

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+"""
+    The ``MyList`` module
+"""
+
+
 class BaseGeometry:
+    """ BaseGeometry class """
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        else:
-            return
 
 class Rectangle(BaseGeometry):
+    """ Rectangle class """
     def __init__(self, width, height):
+        """func is docced"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
