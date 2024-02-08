8c0195a83a269ecf292b0e553872ce76ee4973d4
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 04:50:05 -0800

    Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    The ``Geometry`` module.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
