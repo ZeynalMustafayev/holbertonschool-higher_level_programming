2088e09a6f2ab1f98f54017b9830409e0077ed22
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 00:51:00 -0800

    delete else

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,5 +13,7 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
