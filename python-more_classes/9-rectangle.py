06802ba60a3506fe3fdcdf580da5e03cb602262f
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 09:30:55 -0800

    fix error

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -20,9 +20,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -33,7 +33,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
