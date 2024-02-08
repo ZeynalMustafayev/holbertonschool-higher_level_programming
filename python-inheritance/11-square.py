74d5ec28c6a2c981ed89ab0c6354ec731dd106b1
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 04:50:56 -0800

    fix module error

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,6 +8,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+        Square class that inherits from Rectangle
+    """
     def __init__(self, size):
         super().integer_validator("size", size)
         self.__size = size
