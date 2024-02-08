52055e6f355b9ad2f311034a9867e1f457b1603a
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 04:38:22 -0800

    fix error

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,25 +4,7 @@
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """ Rectangle class """
-    def __init__(self, width, height):
-        """func is docced"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """func is docced"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """For print return messsage"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square class"""
