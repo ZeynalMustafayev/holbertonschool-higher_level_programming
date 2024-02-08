161a4ed9464cc2fa7a84b0283ca00059d45baadc
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-08 05:44:13 -0800

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            for line in file:
+                print(line, end="")
+    except:
+        pass
