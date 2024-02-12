8100777b25e7778bc40a18bf9cb0ad48ddedb326
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-12 04:33:10 -0800

    __str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,6 @@ class Rectangle(Base):
             for j in range(self.width):
                 print("#", end="")
             print("")
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.height, self.width)
