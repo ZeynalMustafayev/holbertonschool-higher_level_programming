d5e92a995bc4ff98fc4a3a24c1fc0996735ec031
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-12 01:19:54 -0800

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""First Rectangle"""
+
+class Base:
+    """Class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+class Rectangle(Base):
+    """Rectangle Class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        self.__width = width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        self.__height = height
+
+    @height.setter
+    def width(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        self.__width = width
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        self.__width = width
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
