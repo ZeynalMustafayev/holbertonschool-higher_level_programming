dca11587a834534b10c4f95a914da72a5b297536
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-13 05:06:54 -0800

    And now, the Square!

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""And now, the Square!"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
