91165d6aace2c9677269cef7f00418d6f4ffb830
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-13 02:08:34 -0800

    Update #0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,17 @@ class Rectangle(Base):
     def __str__(self):
         return "[Rectangle] ({}) {}\
 /{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+
+        return args
