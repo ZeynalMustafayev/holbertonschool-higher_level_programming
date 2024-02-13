9aaccd42fe417b74df6e30268103f70c30889be5
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-13 05:20:20 -0800

    fix overloaded problem

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
     def __str__(self):
         return "[Square] ({}) {}\
-/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+/{} - {}".format(self.id, self.x, self.y, self.width)
