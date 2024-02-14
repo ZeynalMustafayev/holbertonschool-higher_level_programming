ffe912dda48d6bb1d2150b209279ac9f5f1fa39e
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-14 00:32:45 -0800

    fix static method error

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """to_json_string function"""
         if list_dictionaries is None:
