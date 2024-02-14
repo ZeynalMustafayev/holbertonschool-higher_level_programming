466f389167c7c46ee327ae7abba31ea888525144
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-14 01:41:02 -0800

    JSON string to dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -34,3 +34,11 @@ class Base:
             else:
                 my_list = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(my_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or "":
+            return []
+        else:
+            return json_string
