fb1a31f0a9f3df8dc17c9ad0f76e269f7b89cb77
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-02-18 08:33:27 -0800

    fix pep9 error

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -3,15 +3,14 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    
+
     total_weighted_sum = 0
     total_weight = 0
-    
     for score, weight in my_list:
         total_weighted_sum += score * weight
         total_weight += weight
 
     if total_weight == 0:
         return 0
-    
+
     return total_weighted_sum / total_weight
