1ae2eaa597f2919069387d1303a71ff568d0dbb6
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-03-24 19:19:25 +0400

    fix pep8 error

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,8 +10,6 @@ Base = declarative_base()
 
 class State(Base):
     """State class for MySQL database"""
-
-
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
