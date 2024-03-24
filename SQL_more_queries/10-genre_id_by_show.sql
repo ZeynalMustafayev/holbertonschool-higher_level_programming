b561edd0458244cfb043889564262ec36bbe2fa0
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-03-24 17:17:32 +0400

    fix error

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,4 @@
--- duben
--- SELECT s.title, g.genre_id FROM tv_shows s INNER JOIN tv_show_genres g ON s.id = g.show_id WHERE s.id IN (SELECT show_id FROM tv_show_genres GROUP BY show_id HAVING genre_id >= 1) ORDER BY s.title, g.genre_id;
-SELECT s.title, g.genre_id FROM tv_show_genres g LEFT JOIN tv_shows s ON s.id = g.show_id ORDER BY s.title, g.genre_id;i
+-- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
