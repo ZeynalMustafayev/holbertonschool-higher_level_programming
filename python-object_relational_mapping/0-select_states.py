c7eae1118dafea4ee02853fa4c3f12829a6f503e
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-03-24 18:32:03 +0400

    fix pep8 error

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        [print(state) for state in cur.fetchall()]
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in cur.fetchall()]
