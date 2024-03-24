058991e6429ef08940b8eae40551d9c4ad8c1f50
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-03-24 19:52:38 +0400

    fix pep8 errors

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,14 +8,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                                .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                                pool_pre_ping=True)
+     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        state = session.query(State).where(State.id == 2).first()
-        state.name = "New Mexico"
-        session.commit()
-        print(state.id)
-        session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).where(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
+    print(state.id)
+    session.close()
