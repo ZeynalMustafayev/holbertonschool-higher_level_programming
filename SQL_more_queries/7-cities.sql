c60f447900136a76af15ba32b439b50040e3aeda
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-03-24 16:57:19 +0400

    7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- cities
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id int NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id));
