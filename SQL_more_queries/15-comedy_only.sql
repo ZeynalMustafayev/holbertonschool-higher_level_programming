fbdefcae7ae4e6dc814e475ca1f225315a4f8850
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-03-24 17:19:07 +0400

    hot fix

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,6 @@
---comedy only
-SELECT s.title AS title FROM tv_shows s INNER JOIN tv_show_genres sg ON s.id = sg.show_id INNER JOIN tv_genres g ON g.id = sg.genre_id WHERE g.name = 'Comedy' ORDER BY s.title ASC;
+-- Script that lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,5 @@
---shows by genre
-SELECT s.title, g.name FROM tv_shows s LEFT JOIN tv_show_genres sg ON s.id = sg.show_id LEFT JOIN tv_genres g ON g.id = sg.genre_id ORDER BY 1, 2
-ASC;
+-- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
