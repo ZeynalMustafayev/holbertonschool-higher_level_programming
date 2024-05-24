317f3bc9c766d456ed21db2378cb94db936d920b
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-24 19:28:03 +0400

    Add  class

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,3 +1,3 @@
 document.getElementById('red_header').addEventListener('click', function () {
-  document.querySelector.style.color = '#FF0000';
+  document.querySelector('header').style.color = '#FF0000';
 });
